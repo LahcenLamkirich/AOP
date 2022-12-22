f4f489664bb50287534fdd7977724b390b2ecbbd
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-12-22 19:03:51 +0100

    new commit

--- src/main/java/Aspects/SecondAspect.java
+++ src/main/java/Aspects/SecondAspect.java
@@ -1,9 +1,8 @@
 package Aspects;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 
 @Aspect
 public class SecondAspect {
@@ -11,14 +10,27 @@ public class SecondAspect {
     @Pointcut("execution(* Main.Application.main(..))")
     public void pc1(){}
 
-    @Before("pc1()")
-    public void beforeMain(){
-        System.out.println("This is the before Main using the second Aspect !");
-    }
+//    @Before("pc1()")
+//    public void beforeMain(){
+//        System.out.println("This is the before Main using the second Aspect !");
+//    }
+//
+//    @After("pc1()")
+//    public void afterMain(){
+//        System.out.println("This is the After Main using the second Aspect !");
+//    }
 
-    @After("pc1()")
-    public void afterMain(){
-        System.out.println("This is the After Main using the second Aspect !");
-    }
 
+    @Around("pc1()")
+    public void aroundMain(ProceedingJoinPoint joinPoint){
+        System.out.println("This is before in second Aspect Using Around !!");
+        try {
+            joinPoint.proceed();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("This is the After in second Aspect Using Around !!");
+    }
 }
