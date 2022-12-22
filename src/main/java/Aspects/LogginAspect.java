868807ea634c8dc2cde542eb0e01df40db321f05
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-12-22 20:25:15 +0100

    new commit

--- src/main/java/Aspects/LogginAspect.java
+++ src/main/java/Aspects/LogginAspect.java
@@ -1,15 +1,18 @@
 package Aspects;
 
+import jdk.jfr.internal.jfc.model.SettingsLog;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
+import java.util.logging.Logger;
+
 @Aspect
 public class LogginAspect {
 
-    // La duree de l'exeuction :
+    Logger logger = Logger.getLogger(LogginAspect.class.getName());
     Long t1, t2  ;
     @Pointcut("execution(* Metier.*.*(..))") // Toutes les methodes de Metiter :
     public void pc1(){}
@@ -17,13 +20,13 @@ public class LogginAspect {
     @Before("pc1()")
     public void beforeApp(JoinPoint joinPoint){
         t1 = System.currentTimeMillis();
-        System.out.println("Before The execution of the Method : " + joinPoint.getSignature());
+        logger.info("Before The execution of the Method : " + joinPoint.getSignature());
     }
 
     @After("pc1()")
     public void AfterApp(JoinPoint joinPoint){
-        System.out.println("After The execution of the Method : " + joinPoint.getSignature());
+        logger.info("After The execution of the Method : " + joinPoint.getSignature());
         t2 = System.currentTimeMillis();
-        System.out.println("La durree de l'execution de la methode " + joinPoint.getSignature() + " est : " + (t2-t1));
+        logger.info("La durree de l'execution de la methode " + joinPoint.getSignature() + " est : " + (t2-t1));
     }
 }
